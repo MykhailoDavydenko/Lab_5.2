25101fbc7317fe051cd66c86cbcc9a8b88efa3ad
Mykhailo <mykhailo.davydenko.ik.2023@lpnu.ua>
2023-10-18 18:20:55 +0300

    dfghjkl;

---
+++ Lab_5.2/Lab_5.2.cpp
@@ -0,0 +1,84 @@
+﻿// Лабораторна робота № 5.2
+// Давиденко Михайло
+//  Обчислення суми ряду Тейлора за допомогою функцій
+// Варіант 7
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+void S(const double x, const double eps, int& n, double& s);
+void A(const double x, const int n, double& a);
+
+int main()
+{
+	double xp, xk, x, dx, eps, a = 0, R = 0, s = 0;
+	int n = 0;
+	double Pi = 4 * atan(1.0);
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-----------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(12) << "arcctan(x)" << " |"
+		<< setw(10) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-----------------------------------------" << endl;
+	x = xp;
+	double arcctg;
+
+	while (x <= xk)
+	{
+		/*n = 0; // вираз залежить від умови завдання варіанту
+		a = -x; // вираз залежить від умови завдання варіанту
+		S = a;
+		do {
+			n++;
+			R = -(2.0 * n - 1) * x * x / (2.0 * n + 1); // вираз залежить від умови завдання варіанту
+			a *= R;
+			S += a;
+		} while (abs(a) >= eps);
+		*/
+		S(x, eps, n, s);
+		if (x > 0)
+			arcctg = atan(1. / x);
+		else
+			arcctg = Pi + atan(1 / x);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << arcctg << " |"
+			<< setw(10) << setprecision(5) << Pi / 2 + s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-----------------------------------------" << endl;
+	/*double e = 1.0;
+	int i = 2;
+	double q = -0.5;
+	A(e, i, q);
+	cout << q;*/
+	return 0;
+}
+void S(const double x, const double eps, int& n, double& s)
+{
+	n = 0; // вираз залежить від умови завдання варіанту
+	double a = -x; // вираз залежить від умови завдання варіанту
+	s = a;
+	do {
+		n++;
+		A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+}
+
+void A(const double x, const int n, double& a)
+{
+	double R = -(2.0 * n - 1) * x * x / (2.0 * n + 1); // вираз залежить від умови завдання варіанту
+	a *= R;
+}
+
+
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab_5.2/Lab_5.2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+namespace UnitTest2
+{
+    TEST_CLASS(AFunctionTest)
+    {
+    public:
+
+        TEST_METHOD(TestAFunction)
+        {
+            double x = 1.0;
+            int n = 2;
+            double a = -0.5;
+
+            A(x, n, a);
+     
+
+            // Перевірка, чи функція A правильно обчислила a для заданих значень x і n
+            Assert::AreEqual(a, 0.3);
+        }
+
+        // Додайте інші тести за потреби
+    };
+}
